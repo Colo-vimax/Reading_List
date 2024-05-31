9a9a58644214188f518aac74ca09ed2cae5db72c
Colo-vimax <collinsmaganga096@gmail.com>
2024-05-31 17:39:31 +0300

    firebase queries

--- src/hooks/useCollection.jsx
+++ src/hooks/useCollection.jsx
@@ -1,21 +1,30 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { db } from '../firebase/config';
 // firebase imports
-import { collection, onSnapshot } from 'firebase/firestore';
+import { collection, onSnapshot, query, where } from 'firebase/firestore';
 
-const useCollection = (c) => {
+const useCollection = (c,_q) => {
     const [documents, setDocuments] = useState(null);
+
+    // SETUP QUERY(ARRAY IF CHANGES).VALUE
+    const q = useRef(_q).current
+
 
     useEffect(() => {
         // Check if the collection path is provided
         if (!c) {
             console.error('Collection path is empty');
-            return;
+            return; 
         }
 
         // SPECIFIC COLLECTION
         let ref = collection(db, c);
-
+        
+        // OPTIONALLY PARAMS(ARRAY)
+        if(q){
+           ref = query(ref, where(...q))
+        }
+   
         // AWHILE
         const unsub = onSnapshot(ref, (snapshot) => {
             let results = [];
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React, { useState } from 'react'
 import useCollection from '../hooks/useCollection'
+import { useAuthContext } from '../hooks/useAuthContext';
 
 import BookList from '../components/BookList'
 import BookForm from '../components/BookForm'
 
-
 const Home = () => {
-  const { documents:books } = useCollection('books')
+  const { user } = useAuthContext()
+  // FETCH MY BOOKS
+  const { documents:books } = useCollection("books",["uid", "==", user.uid]);
 
   return (
     <div className='App'>
